a783d912c0e20a700f54abdce2bdba4e7305c372
Athyuttam Reddy <athyuttamre@gmail.com>
2016-04-01 00:53:52 -0400

    Rename gitignore_global, add path

--- install.sh
+++ install.sh
@@ -1,1 +1,33 @@
 #!/usr/bin/env bash
+
+# install.sh: makes everything happen
+# Source: @webpro
+
+# Get current dir (so this script can be run from anywhere)
+export DOTFILES_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Update dotfiles itself first
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+# Bunch of symlinks
+ln -sfv "$DOTFILES_DIR/.aliases" ~
+ln -sfv "$DOTFILES_DIR/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/.bash_prompt" ~
+ln -sfv "$DOTFILES_DIR/.bashrc" ~
+ln -sfv "$DOTFILES_DIR/.editorconfig" ~
+ln -sfv "$DOTFILES_DIR/.exports" ~
+ln -sfv "$DOTFILES_DIR/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/.gitignore_global" ~
+ln -sfv "$DOTFILES_DIR/.hushlogin" ~
+ln -sfv "$DOTFILES_DIR/.inputrc" ~
+ln -sfv "$DOTFILES_DIR/.path" ~
+
+# Package managers & packages
+. "$DOTFILES_DIR/install/brew.sh"
+. "$DOTFILES_DIR/install/bash.sh"
+. "$DOTFILES_DIR/install/golang.sh"
+. "$DOTFILES_DIR/install/ruby.sh"
+. "$DOTFILES_DIR/install/python.sh"
+. "$DOTFILES_DIR/install/node.sh"
+. "$DOTFILES_DIR/install/meteor.sh"
