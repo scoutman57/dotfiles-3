32e9827a55c606fc4138e33da39b90784fab0e79
Athyuttam Reddy <athyuttamre@gmail.com>
2016-05-04 00:08:19 -0400

    Add tmux to brew file

--- install/brew.sh
+++ install/brew.sh
@@ -28,6 +28,8 @@ brew install moreutils
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
+# Install `tmux`
+brew install tmux
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
